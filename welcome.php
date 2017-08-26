38216c1d0476f681f46e5933c98d8e4d4ab35246
Anushka Mehra <hrms.anushkam@gmail.com>
2017-08-26 07:19:30 +0530

    Delete welcome.php

--- welcome.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-    <head></head>
-    <body>
-        <section>
-            <h1>Welcome!<?php echo $_SESSION['f_name']; ?></h1>
-            <a href="_/includes/session_unset.php">Logout!</a>
-        </section>
-    </body>
-</html>
